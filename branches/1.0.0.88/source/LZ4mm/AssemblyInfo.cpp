39359f3d128dea82b79fe1093ffb6baf2e1925db
Krashan_cp <SND\Krashan_cp>
2013-02-25 09:45:05 -0800

    stable release 1.0.0.88

---
+++ branches/1.0.0.88/source/LZ4mm/AssemblyInfo.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+//
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly:AssemblyTitleAttribute("LZ4mm")];
+[assembly:AssemblyDescriptionAttribute("LZ4 compression codec (MixedMode)")];
+[assembly:AssemblyConfigurationAttribute("")];
+[assembly:AssemblyCompanyAttribute("Milosz Krajewski")];
+[assembly:AssemblyProductAttribute("LZ4mm")];
+[assembly:AssemblyCopyrightAttribute("Copyright (c) 2013, Milosz Krajewski")];
+[assembly:AssemblyTrademarkAttribute("")];
+[assembly:AssemblyCultureAttribute("")];
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the value or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+
+[assembly:AssemblyVersionAttribute("1.0.0.88")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
+
+[assembly:SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
---
+++ branches/1.0.0.88/source/adapters/cpp/lz4_64.cpp
@@ -0,0 +1,4 @@
+// This file imports lz4.c prefixing all functions with LIB64_
+
+#include "lz4_64.h"
+#include "..\..\..\original\lz4.c"
