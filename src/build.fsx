b41949a1cbc536d24e20c53cab3440ec579e08a1
unknown <KrajewskiM@SEPURA1166.SEPURA.co.uk>
2015-09-22 16:59:47 +0100

    fake + paket

--- src/build.fsx
+++ src/build.fsx
@@ -1,11 +1,21 @@
 #r "packages/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.Testing
+open StrongNamingHelper
 
 let outDir = "./../out"
 let testDir = outDir @@ "test"
 let buildDir = outDir @@ "build"
 let releaseDir = outDir @@ "release"
+let snk = "LZ4.snk"
+
+let testFile fn = (fileInfo fn).Exists
+
+Target "KeyGen" (fun _ ->
+    match testFile snk with
+    | true -> ()
+    | _ -> snk |> sprintf "-k %s" |> StrongName id
+)
 
 Target "Clean" (fun _ ->
     !! "**/bin" ++ "**/obj" |> CleanDirs
@@ -17,5 +27,7 @@ Target "Build" (fun _ ->
     |> MSBuildRelease buildDir "Build"
     |> Log "Build-Output: "
 )
+
+"KeyGen" ==> "Build"
 
 RunTargetOrDefault "Build"
