16a9a910b0cbde67265ed48a6be9a7df4f2c5e82
Milosz Krajewski <krajewskim@SEPURA1166.SEPURA.co.uk>
2016-02-25 11:14:58 +0000

    cleanup

--- src/LZ4/LZ4Codec.cs
+++ src/LZ4/LZ4Codec.cs
@@ -172,9 +172,7 @@ namespace LZ4
 		private static ILZ4Service AutoTest(ILZ4Service service)
 		{
 			const string loremIpsum =
-				"Lorem ipsum dolor sit amet, consectetur adipis" +
-				"" +
-				"icing elit, sed do eiusmod tempor incididunt ut " +
+				"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut " +
 				"labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
 				"laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " +
 				"voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
